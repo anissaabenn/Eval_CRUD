c0158b95ac31128f619b6e225c75651f33422f3f
anissaabenn <107192853+anissaabenn@users.noreply.github.com>
2022-12-20 20:50:27 +0100

    Affichage des données de la table PRODUCTS dans un tableau

--- index.php
+++ index.php
@@ -10,4 +10,40 @@ $result = $query->fetchAll(PDO::FETCH_ASSOC);
 require_once('close.php');
 ?>
 
+<main class="contrainer">
+    <div class="row">
+        <section class="col-12">
+            <h1>Liste des produits</h1>
+            <table class="table">
+                <thead>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Description</th>
+                    <th>Prix</th>
+                    <th>Categorie</th>
+                    <th>Quantitée</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach($result as $produit){
+                    ?>
+                    <tr>
+                        <td><?= $produit['id'] ?></td>
+                        <td><?= $produit['name'] ?></td>
+                        <td><?= $produit['description'] ?></td>
+                        <td><?= $produit['price'] ?></td>
+                        <td><?= $produit['category'] ?></td>
+                        <td><?= $produit['quantity'] ?></td>
+                        <td></td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+    </section>
+    </div>
+</main>
+
 <?php require_once('view/footer.view.php') ?>
